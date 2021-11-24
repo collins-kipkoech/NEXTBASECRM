a45e52ab751a1db640cd543da2f8ab09796b590a
collins <collinskipkoech95@gmail.com>
2021-11-24 10:43:02 +0300

    find search web element, created a search constructor and also created searching method for searching given parameter

--- src/test/java/com/CRM/pages/Search.java
+++ src/test/java/com/CRM/pages/Search.java
@@ -1,5 +1,22 @@
 package com.CRM.pages;
 
+import com.CRM.utility.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Search {
-    
+    @FindBy(xpath = "//input[@name='q']")
+    public WebElement searchBtn;
+
+    public Search(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    public void searching(String word){
+        searchBtn.sendKeys(word);
+    }
+
+
+
 }
