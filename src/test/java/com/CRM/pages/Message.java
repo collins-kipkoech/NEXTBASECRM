af7bd0e76e2e9a5f3a06a3797294004960ac3ed4
collins <collinskipkoech95@gmail.com>
2021-12-01 14:26:35 +0300

    identified the iframe web element to use it switch to the iframe

--- src/test/java/com/CRM/pages/Message.java
+++ src/test/java/com/CRM/pages/Message.java
@@ -6,4 +6,7 @@ import org.openqa.selenium.support.FindBy;
 public class Message {
     @FindBy(xpath = "//span[@class='feed-add-post-micro-title']")
     public WebElement messageBtn;
+
+    @FindBy(xpath = "//iframe[@class='bx-editor-iframe']")
+    public WebElement iframeBtn;
 }
